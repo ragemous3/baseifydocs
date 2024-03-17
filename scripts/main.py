a8840015cd10c95fc0d8ce22e8a5b3a908507ce7
Rasmus Moberg <rasmus.krister.moberg@gmail.com>
2024-03-17 19:56:04 +0100

    bug issue, need to go recursive

--- scripts/main.py
+++ scripts/main.py
@@ -15,9 +15,14 @@ keep_attributes = set(["href", "src"])
 def main(url: str = test):
 	with urlopen(url) as webpage:
 		content = webpage.read().decode()
-		
-		out(clean(content))
+
+		for i in range(0, 3):
+			content = clean(content)
+
+		out(content)
 		return 1
+
+#Not recursive, content will have empty divs etc after this is done. No good.
 
 def clean(html):
 	soup = BeautifulSoup(html, "html.parser")
@@ -31,18 +36,15 @@ def clean(html):
 			item.decompose()
 			continue
 
-		# If element has no parent and is a div, remove. 
-		if not item.parent and item.name == "div":
-			item.decompose()
-			continue
-		
-		if len(list(item.children)) == 0:
+		# Bug, it removes too much
+		if not item.findAll(True):
 			item.decompose()
 			continue
 
 		if not item.get_text():
 			item.decompose()
 			continue
+
   		# If the element is div, if the elemet has 1 child and if the that element is a div, remove it.
 		if item.name == "div" and len(list(item.contents)) == 1 and list(item.contents)[0].name == "div":
 			for kid in item.children:
@@ -50,11 +52,11 @@ def clean(html):
 			item.decompose()
 			continue
    
-	return soup
+	return soup.prettify()
 
 def out(soup):
 	with open("output/out.html", "w") as o:
-		o.write(soup.prettify())
+		o.write(soup)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog="baseifydocs", description="Used for taking snippets of docs and downloading it")
@@ -62,3 +64,6 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	main(args.url)
 
+""" 		if not item.parent and item.name == "div":
+			item.decompose()
+			continue """
